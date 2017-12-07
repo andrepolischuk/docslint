040d6f05cbfd3562a50e83b2b8335f986ff69339
Andrey Polischuk <me@andrepolischuk.com>
2017-12-07 16:43:44 +0300

    Fix tests on node < 8

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import test from 'ava'
-import fs from 'fs'
 import execa from 'execa'
+import fs from 'fs-extra'
 
 test('should lint', async t => {
   const {stdout} = await execa('./cli.js', ['./fixture.md'])
@@ -11,11 +11,11 @@ test('should lint', async t => {
 })
 
 test('should fix', async t => {
-  fs.copyFileSync('./fixture.md', './temp-fixture.md')
+  await fs.copy('./fixture.md', './temp-fixture.md')
   const {stdout} = await execa('./cli.js', ['./temp-fixture.md', '--fix'])
   t.true(stdout.indexOf('http://andrepolischuk.com is redirected') > 0)
   t.true(stdout.indexOf('Correct it to misspelling') > 0)
   t.true(stdout.indexOf('use “Markdown” instead') > 0)
   t.true(stdout.indexOf('Avoid using “Currently” ') === -1)
-  fs.unlinkSync('./temp-fixture.md')
+  await fs.remove('./temp-fixture.md')
 })
